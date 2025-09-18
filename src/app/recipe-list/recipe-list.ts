0dd455331440b264167cf3399e282a7bc3b67a39
ronder.lee <ronder.lee@gmail.com>
2025-09-18 06:22:56 +0000

    feat. 建立可搜尋的食譜列表

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -3,21 +3,29 @@ import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeModel } from '../models';
 import { JsonPipe } from '@angular/common';
 import { RecipeDetail } from "../recipe-detail/recipe-detail";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail,FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   protected recipes = MOCK_RECIPES;
 
+  protected readonly searchTerm = signal<string>('');
+
+  protected readonly filteredRecipes = computed(() => {
+    const search = this.searchTerm().toLowerCase();
+    return this.recipes.filter(recipe => recipe.name.toLowerCase().includes(search));
+  });
+
   protected readonly recipe = signal<RecipeModel | undefined>(MOCK_RECIPES[0]);
   
   
 
-  show(recipe: RecipeModel) {
+  selectRecipe(recipe: RecipeModel) {
     this.recipe.set(recipe);
   }
 
